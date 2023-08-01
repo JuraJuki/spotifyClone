4dfda1034f6043313a0997c0fd4ceb79fe75be9e
JuraJuki <juraypavlovic@gmail.com>
2023-08-01 20:40:07 +0200

    finish song and image upload

--- components/UploadModal/useUploadModalSubmit.ts
+++ components/UploadModal/useUploadModalSubmit.ts
@@ -1,37 +1,19 @@
 "use client";
-import Button from "@/components/Button";
-import Input from "@/components/Input";
-import Modal from "@/components/Modal";
 import { useUploadModal } from "@/hooks/useUploadModal";
 import { useUser } from "@/hooks/useUser";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/navigation";
-import { FC, useState } from "react";
-import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { useState } from "react";
+import { FieldValues, SubmitHandler } from "react-hook-form";
 import toast from "react-hot-toast";
 import uniqid from "uniqid";
 
-const UploadModal: FC = () => {
+export const useUploadModalSubmit = (restForm: () => void) => {
   const [isLoading, setIsLoading] = useState(false);
-  const uploadModal = useUploadModal();
   const { user } = useUser();
   const supabaseClient = useSupabaseClient();
   const router = useRouter();
-
-  const { register, handleSubmit, reset } = useForm<FieldValues>({
-    defaultValues: {
-      author: "",
-      title: "",
-      song: null,
-      image: null,
-    },
-  });
-  const onChange = (open: boolean) => {
-    if (!open) {
-      reset();
-      uploadModal.onClose();
-    }
-  };
+  const uploadModal = useUploadModal();
 
   const onSubmit: SubmitHandler<FieldValues> = async (values) => {
     try {
@@ -90,7 +72,7 @@ const UploadModal: FC = () => {
       router.refresh();
       setIsLoading(false);
       toast.success("Song created!");
-      reset();
+      restForm();
       uploadModal.onClose();
     } catch (error) {
       toast.error("Something went wrong");
@@ -99,55 +81,5 @@ const UploadModal: FC = () => {
     }
   };
 
-  return (
-    <Modal
-      title="Add a song"
-      description="Upload an mp3 file"
-      isOpen={uploadModal.isOpen}
-      onChange={onChange}
-    >
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className={"flex flex-col gap-y-4"}
-      >
-        <Input
-          id={"title"}
-          disabled={isLoading}
-          {...register("title", { required: true })}
-          placeholder={"Song title"}
-        />
-        <Input
-          id={"author"}
-          disabled={isLoading}
-          {...register("author", { required: true })}
-          placeholder={"Song author"}
-        />
-        <div>
-          <div className={"pb-1"}>Select a song file</div>
-          <Input
-            id={"song"}
-            type={"file"}
-            disabled={isLoading}
-            accept={".mp3"}
-            {...register("song", { required: true })}
-          />
-        </div>
-        <div>
-          <div className={"pb-1"}>Select an image</div>
-          <Input
-            id={"image"}
-            type={"file"}
-            disabled={isLoading}
-            accept={"image/*"}
-            {...register("image", { required: true })}
-          />
-        </div>
-        <Button disabled={isLoading} type={"submit"}>
-          Create
-        </Button>
-      </form>
-    </Modal>
-  );
+  return { onSubmit, isLoading };
 };
-
-export default UploadModal;
--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 import AuthModal from "@/components/AuthModal";
-import UploadModal from "@/components/UploadModal";
+import UploadModal from "@/components/UploadModal/UploadModal";
 import { FC, useEffect, useState } from "react";
 
 export const ModalProvider: FC = () => {
